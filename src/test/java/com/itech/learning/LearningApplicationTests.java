6151cad7e0463fc90ed017b9e54a1c932582e519
lemonclouds <hannah.lakhai@gmail.com>
2022-02-17 15:25:13 +0300

    Initial commit

---
+++ src/test/java/com/itech/learning/LearningApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itech.learning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearningApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
