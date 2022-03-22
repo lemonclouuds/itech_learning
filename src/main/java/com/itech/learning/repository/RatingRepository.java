508e6fdb41eb4636d42b1ddb0efb67ad59e4d069
lemonclouds <hannah.lakhai@gmail.com>
2022-03-22 11:21:40 +0300

    Added deleteByIdIn() for domain repositories

--- src/main/java/com/itech/learning/repository/RatingRepository.java
+++ src/main/java/com/itech/learning/repository/RatingRepository.java
@@ -3,9 +3,11 @@ package com.itech.learning.repository;
 import com.itech.learning.domain.Rating;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Collection;
 import java.util.List;
 
 public interface RatingRepository extends JpaRepository<Rating, Long> {
     List<Rating> findAllByUserId(Long id);
     List<Rating> findAllByLessonId(Long id);
+    void deleteByIdIn(Collection<Long> ids);
 }
--- src/main/java/com/itech/learning/repository/UserRepository.java
+++ src/main/java/com/itech/learning/repository/UserRepository.java
@@ -4,9 +4,11 @@ import com.itech.learning.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Collection;
 import java.util.Optional;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findUserByUsername(String username);
+    void deleteByIdIn(Collection<Long> ids);
 }
