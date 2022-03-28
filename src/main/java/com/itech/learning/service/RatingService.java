4357127762df638659e93db15f60d1d30c105ff9
lemonclouds <hannah.lakhai@gmail.com>
2022-03-28 13:56:48 +0300

    Add DTO to domain services

--- src/main/java/com/itech/learning/service/RatingService.java
+++ src/main/java/com/itech/learning/service/RatingService.java
@@ -3,10 +3,12 @@ package com.itech.learning.service;
 import com.itech.learning.domain.Lesson;
 import com.itech.learning.domain.Rating;
 import com.itech.learning.domain.User;
+import com.itech.learning.domain.dto.RatingDto;
 import com.itech.learning.repository.LessonRepository;
 import com.itech.learning.repository.RatingRepository;
 import com.itech.learning.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -19,9 +21,12 @@ import static com.itech.learning.service.ExceptionMessage.*;
 @Service
 @RequiredArgsConstructor
 public class RatingService {
+    private static ModelMapper modelMapper = new ModelMapper();
+
     private final RatingRepository ratingRepository;
     private final UserRepository userRepository;
     private final LessonRepository lessonRepository;
+    //private final LessonService lessonService;
 
     public List<Rating> getAll() {
         return ratingRepository.findAll();
@@ -32,7 +37,7 @@ public class RatingService {
                 () -> new EntityNotFoundException(String.format(RATING_WITH_ID_NOT_FOUND, ratingId)));
     }
 
-    public Rating addRating(Double rate, Long userId, Long lessonId) {
+    /*public Rating addRating(Double rate, Long userId, Long lessonId) {
         User user = userRepository.findById(userId).orElseThrow(
                 () -> new EntityNotFoundException(String.format(USER_WITH_ID_NOT_FOUND, userId)));
         Lesson lesson = lessonRepository.findById(lessonId).orElseThrow(
@@ -44,13 +49,24 @@ public class RatingService {
         }
 
         return rating;
-    }
+    }*/
 
-    public Rating updateRating(Long ratingId, Double rate) {
-        Rating rating = findById(ratingId);
-        rating.setRate(rate);
+    @Transactional
+    public RatingDto update(RatingDto ratingDto) {
+        Rating rating = modelMapper.map(ratingDto, Rating.class);
+        User user = userRepository.findById(ratingDto.getUserId()).orElseThrow(
+                () -> new EntityNotFoundException(String.format(USER_WITH_ID_NOT_FOUND, ratingDto.getUserId())));
+        Lesson lesson = lessonRepository.findById(ratingDto.getLessonId()).orElseThrow(
+                () -> new EntityNotFoundException(String.format(LESSON_WITH_ID_NOT_FOUND, ratingDto.getLessonId()))
+        );
+        //Lesson lesson = lessonService.findById(ratingDto.getLessonId());
+        // Relying upon circular references is discouraged and they are prohibited by default.
+
+        rating.setUser(user);
+        rating.setLesson(lesson);
         ratingRepository.save(rating);
-        return rating;
+
+        return modelMapper.map(rating, RatingDto.class);
     }
 
     @Transactional
