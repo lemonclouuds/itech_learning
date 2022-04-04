b6d5b676849ddf8a136f5fab526c0c6a3f35011b
lemonclouds <hannah.lakhai@gmail.com>
2022-04-04 13:00:11 +0300

    Add EntityAlreadyExistsException

---
+++ src/main/java/com/itech/learning/exception/EntityAlreadyExistsException.java
@@ -0,0 +1,10 @@
+package com.itech.learning.exception;
+
+public class EntityAlreadyExistsException extends RuntimeException{
+    public EntityAlreadyExistsException() {
+    }
+
+    public EntityAlreadyExistsException(String message) {
+        super(message);
+    }
+}
