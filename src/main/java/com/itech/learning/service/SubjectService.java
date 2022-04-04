b699c041a617dbfdb43cadfb09072c42ff0a3f05
lemonclouds <hannah.lakhai@gmail.com>
2022-04-04 12:59:56 +0300

    Implement methods in SubjectService to work w/ dtos

--- src/main/java/com/itech/learning/service/SubjectService.java
+++ src/main/java/com/itech/learning/service/SubjectService.java
@@ -1,7 +1,12 @@
 package com.itech.learning.service;
 
+import com.itech.learning.domain.Rating;
 import com.itech.learning.domain.Subject;
+import com.itech.learning.domain.User;
+import com.itech.learning.domain.dto.RatingDto;
 import com.itech.learning.domain.dto.SubjectDto;
+import com.itech.learning.exception.EntityAlreadyExistsException;
+import com.itech.learning.helper.MapperHelper;
 import com.itech.learning.repository.SubjectRepository;
 import lombok.RequiredArgsConstructor;
 import org.modelmapper.ModelMapper;
@@ -11,8 +16,10 @@ import org.springframework.transaction.annotation.Transactional;
 import javax.persistence.EntityNotFoundException;
 import java.util.Collection;
 import java.util.List;
+import java.util.Optional;
 
-import static com.itech.learning.service.ExceptionMessage.SUBJECT_WITH_ID_NOT_FOUND;
+import static com.itech.learning.constants.ExceptionMessage.RATING_WITH_ID_NOT_FOUND;
+import static com.itech.learning.constants.ExceptionMessage.SUBJECT_WITH_ID_NOT_FOUND;
 
 @Service
 @RequiredArgsConstructor
@@ -20,17 +27,10 @@ public class SubjectService {
     private static ModelMapper modelMapper = new ModelMapper();
 
     private final SubjectRepository subjectRepository;
+    private final UserService userService;
 
-    public List<Subject> getAll() {
-        return subjectRepository.findAll();
-    }
-
-    @Transactional
-    public Subject updateTitle(Long subjectId, String title) {
-        Subject subject = findById(subjectId);
-        subject.setTitle(title);
-        subjectRepository.save(subject);
-        return subject;
+    public List<SubjectDto> getAll() {
+        return MapperHelper.mapList(subjectRepository.findAll(), SubjectDto.class);
     }
 
     @Transactional
@@ -39,32 +39,40 @@ public class SubjectService {
             Subject subject = new Subject(subjectDto.getTitle(), subjectDto.getSolution());
             subjectRepository.save(subject);
         } else {
-            update(subjectDto);
+            throw new EntityAlreadyExistsException("Entity already exists");
         }
         return subjectDto;
     }
 
     @Transactional
-    public SubjectDto update(SubjectDto subjectDto) {
+    public SubjectDto update(Long subjectId, SubjectDto subjectDto) {
         Subject subject = modelMapper.map(subjectDto, Subject.class);
-        if (!subjectRepository.existsById(subjectDto.getId())) {
-            create(subjectDto);
+        subject.setId(subjectId); //это если не dto.id == null?
+        if (!subjectRepository.existsById(subjectId)) {
+            throw new EntityNotFoundException();
         } else {
-            //List<Lesson> lessons = lessonRepository.findAllBySubjectId(subjectDto.getId());
-            //subject.setLessons(lessons);
             subjectRepository.save(subject);
         }
         return subjectDto;
     }
 
     @Transactional
-    public SubjectDto addLesson(Long subjectId/*, LessonDto lessonDto*/) {
+    public SubjectDto addRating(Long subjectId, RatingDto ratingDto) {
         Subject subject = findById(subjectId);
-        //Lesson lesson = modelMapper.map(lessonDto, Lesson.class);
-        /*if (!subject.getLessons().contains(lesson)) {
-            subject.getLessons().add(lesson);
-        }*/
-        subjectRepository.save(subject);
+        User user = userService.findById(ratingDto.getUserId());
+        Rating rating = modelMapper.map(ratingDto, Rating.class);
+        Optional<Rating> found = subject.getRatings()
+                .stream()
+                .filter(rating1 -> ratingDto.getUserId().equals(rating1.getUser().getId()))
+                .findFirst();
+        if (found.isPresent()) {
+            throw new EntityAlreadyExistsException("User had already rated this subject");
+        } else {
+                subject.getRatings().add(rating);
+                rating.setSubject(subject);
+                rating.setUser(user);
+                subjectRepository.save(subject);
+        }
         return modelMapper.map(subject, SubjectDto.class);
     }
 
@@ -82,13 +90,32 @@ public class SubjectService {
         }
     }
 
+    @Transactional
     public Subject findById(Long subjectId) {
         return subjectRepository.findById(subjectId).orElseThrow(
                 () -> new EntityNotFoundException(String.format(SUBJECT_WITH_ID_NOT_FOUND, subjectId)));
     }
 
-    /*@Transactional
-    public List<Rating> getSubjectRatings(Long subjectId) {
-        return findById(subjectId).getRatings();
-    }*/
+    @Transactional
+    public SubjectDto findDtoById(Long subjectId) {
+        return modelMapper.map(findById(subjectId), SubjectDto.class);
+    }
+
+    @Transactional
+    public List<RatingDto> getSubjectRatings(Long subjectId) {
+        Subject subject = findById(subjectId);
+        return MapperHelper.mapList(subject.getRatings(), RatingDto.class);
+    }
+
+    @Transactional
+    public RatingDto getSubjectRatingById(Long subjectId, Long ratingId) {
+        Rating found = findById(subjectId).getRatings()
+                .stream()
+                .filter(rating -> ratingId.equals(rating.getId()))
+                .findFirst()
+                .orElseThrow(
+                        () -> new EntityNotFoundException(String.format(RATING_WITH_ID_NOT_FOUND, ratingId)));
+        return modelMapper.map(found, RatingDto.class);
+    }
+
 }
