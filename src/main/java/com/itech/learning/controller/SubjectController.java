af5bb3a9b47aaa65c04097ca4e6e31b1e324f2d4
lemonclouds <hannah.lakhai@gmail.com>
2022-04-06 15:13:48 +0300

    Add mappings to SubjectController

--- src/main/java/com/itech/learning/controller/SubjectController.java
+++ src/main/java/com/itech/learning/controller/SubjectController.java
@@ -1,12 +1,12 @@
 package com.itech.learning.controller;
 
-import com.itech.learning.domain.Lesson;
-import com.itech.learning.domain.Subject;
+import com.itech.learning.domain.dto.RatingDto;
+import com.itech.learning.domain.dto.SubjectDto;
+import com.itech.learning.service.RatingService;
 import com.itech.learning.service.SubjectService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -14,19 +14,60 @@ import java.util.List;
 @RequiredArgsConstructor
 public class SubjectController {
     private final SubjectService subjectService;
+    private final RatingService ratingService;
 
     @GetMapping("/subjects")
-    public List<Subject> findAll() {
+    public List<SubjectDto> findAll() {
         return subjectService.getAll();
     }
 
-    @GetMapping("/subjects/{id}")
-    public Subject findById(@PathVariable Long id) {
-        return subjectService.findById(id);
+    @GetMapping("/subjects/{subjectId}")
+    public SubjectDto findById(@PathVariable Long subjectId) {
+        return subjectService.findDtoById(subjectId);
     }
 
-    @GetMapping("/subjects/{id}/lessons")
-    public List<Lesson> findSubjectLessons(@PathVariable Long id) {
-        return subjectService.getSubjectLessons(id);
+    @GetMapping("/subjects/{subjectId}/ratings")
+    public List<RatingDto> getSubjectRatings(@PathVariable Long subjectId) {
+        return subjectService.getSubjectRatings(subjectId);
     }
+
+    @GetMapping("/subjects/{subjectId}/ratings/{ratingId}")
+    public RatingDto getSubjectRatings(@PathVariable Long subjectId, @PathVariable Long ratingId) {
+        return subjectService.getSubjectRatingById(subjectId, ratingId);
+    }
+
+    @PutMapping("/subjects/{subjectId}")
+    public SubjectDto update(@PathVariable Long subjectId, @RequestBody SubjectDto subjectDto) {
+        return subjectService.update(subjectId, subjectDto);
+    }
+
+    @PutMapping("/subjects/{subjectId}/ratings/{ratingId}")
+    public RatingDto updateRating(@PathVariable Long ratingId, @RequestBody RatingDto ratingDto) {
+        return ratingService.update(ratingId, ratingDto);
+    }
+
+    @PostMapping("/subjects")
+    @ResponseStatus(code = HttpStatus.CREATED)
+    SubjectDto create(@RequestBody SubjectDto subjectDto) {
+        return subjectService.create(subjectDto);
+    }
+
+    @PostMapping("/subjects/{subjectId}/ratings")
+    @ResponseStatus(code = HttpStatus.CREATED)
+    public RatingDto createRating(@PathVariable Long subjectId, @RequestBody RatingDto ratingDto) {
+        return subjectService.addRating(subjectId, ratingDto);
+    }
+
+    @DeleteMapping("/subjects/{subjectId}")
+    @ResponseStatus(code = HttpStatus.OK)
+    void delete(@PathVariable Long subjectId) {
+        subjectService.deleteById(subjectId);
+    }
+
+    @DeleteMapping("/subjects/{subjectId}/ratings/{ratingId}")
+    @ResponseStatus(code = HttpStatus.OK)
+    void deleteRating(@PathVariable Long ratingId) {
+        ratingService.deleteById(ratingId);
+    }
+
 }
