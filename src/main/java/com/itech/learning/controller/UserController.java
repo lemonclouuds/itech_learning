35cbd8ac6adc67bae42a81a830d46875d182c17a
lemonclouds <hannah.lakhai@gmail.com>
2022-04-06 15:15:02 +0300

    Add mappings to UserController

--- src/main/java/com/itech/learning/controller/UserController.java
+++ src/main/java/com/itech/learning/controller/UserController.java
@@ -1,12 +1,12 @@
 package com.itech.learning.controller;
 
-import com.itech.learning.domain.Rating;
-import com.itech.learning.domain.User;
+import com.itech.learning.domain.dto.RatingDto;
+import com.itech.learning.domain.dto.UserDto;
+import com.itech.learning.service.RatingService;
 import com.itech.learning.service.UserService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -14,20 +14,43 @@ import java.util.List;
 @RequiredArgsConstructor
 public class UserController {
     private final UserService userService;
+    private final RatingService ratingService;
 
     @GetMapping("/users")
-    public List<User> findAll() {
+    public List<UserDto> getAll() {
         return userService.getAll();
     }
 
-    @GetMapping("/users/{id}")
-    public User findById(@PathVariable Long id) {
-        return userService.findById(id);
+    @GetMapping("/users/{userId}")
+    public UserDto getById(@PathVariable Long userId) {
+        return userService.findDtoById(userId);
     }
 
-    @GetMapping("/users/{id}/ratings")
-    public List<Rating> findUserRatings(@PathVariable Long id) {
-        return userService.getUserRates(id);
+    @GetMapping("/users/{userId}/ratings")
+    public List<RatingDto> getUserRatings(@PathVariable Long userId) {
+        return ratingService.findAllByUserId(userId);
+    }
+
+    @GetMapping("/users/{userId}/ratings/{ratingId}")
+    public RatingDto getUserRatingById(@PathVariable Long userId, @PathVariable Long ratingId) {
+        return userService.getUserRatingById(userId, ratingId);
+    }
+
+    @PutMapping("/users/{userId}")
+    public UserDto update(@PathVariable Long userId, @RequestBody UserDto userDto) {
+        return userService.update(userId, userDto);
+    }
+
+    @PostMapping("/users")
+    @ResponseStatus(code = HttpStatus.CREATED)
+    UserDto create(@RequestBody UserDto userDto) {
+        return userService.create(userDto);
+    }
+
+    @DeleteMapping("/users/{userId}")
+    @ResponseStatus(code = HttpStatus.OK)
+    void delete(@PathVariable Long userId) {
+        userService.deleteById(userId);
     }
 
 }
