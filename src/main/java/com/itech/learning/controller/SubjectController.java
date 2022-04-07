a6a1b72658e974c6d95ed65ada5ef43624519f1a
lemonclouds <hannah.lakhai@gmail.com>
2022-04-07 14:55:31 +0300

    Wrap controller responses in ResponseEntity

--- src/main/java/com/itech/learning/controller/SubjectController.java
+++ src/main/java/com/itech/learning/controller/SubjectController.java
@@ -6,6 +6,7 @@ import com.itech.learning.service.RatingService;
 import com.itech.learning.service.SubjectService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -17,57 +18,63 @@ public class SubjectController {
     private final RatingService ratingService;
 
     @GetMapping("/subjects")
-    public List<SubjectDto> findAll() {
-        return subjectService.getAll();
+    public ResponseEntity<List<SubjectDto>> findAll() {
+        return ResponseEntity.ok(subjectService.getAll());
     }
 
     @GetMapping("/subjects/{subjectId}")
-    public SubjectDto findById(@PathVariable Long subjectId) {
-        return subjectService.findDtoById(subjectId);
+    public ResponseEntity<SubjectDto> findById(@PathVariable Long subjectId) {
+        return ResponseEntity.ok(subjectService.findDtoById(subjectId));
     }
 
     @GetMapping("/subjects/{subjectId}/ratings")
-    public List<RatingDto> getSubjectRatings(@PathVariable Long subjectId) {
-        return subjectService.getSubjectRatings(subjectId);
+    public ResponseEntity<List<RatingDto>> getSubjectRatings(@PathVariable Long subjectId) {
+        return ResponseEntity.ok(subjectService.getSubjectRatings(subjectId));
     }
 
     @GetMapping("/subjects/{subjectId}/ratings/{ratingId}")
-    public RatingDto getSubjectRatings(@PathVariable Long subjectId, @PathVariable Long ratingId) {
-        return subjectService.getSubjectRatingById(subjectId, ratingId);
+    public ResponseEntity<RatingDto> getSubjectRatings(@PathVariable Long subjectId, @PathVariable Long ratingId) {
+        return ResponseEntity.ok(subjectService.getSubjectRatingById(subjectId, ratingId));
     }
 
     @PutMapping("/subjects/{subjectId}")
-    public SubjectDto update(@PathVariable Long subjectId, @RequestBody SubjectDto subjectDto) {
-        return subjectService.update(subjectId, subjectDto);
+    public ResponseEntity<SubjectDto> update(@PathVariable Long subjectId, @RequestBody SubjectDto subjectDto) {
+        return ResponseEntity.ok(subjectService.update(subjectId, subjectDto));
     }
 
     @PutMapping("/subjects/{subjectId}/ratings/{ratingId}")
-    public RatingDto updateRating(@PathVariable Long ratingId, @RequestBody RatingDto ratingDto) {
-        return ratingService.update(ratingId, ratingDto);
+    public ResponseEntity<RatingDto> updateRating(@PathVariable Long ratingId, @RequestBody RatingDto ratingDto) {
+        return ResponseEntity.ok(ratingService.update(ratingId, ratingDto));
     }
 
     @PostMapping("/subjects")
     @ResponseStatus(code = HttpStatus.CREATED)
-    SubjectDto create(@RequestBody SubjectDto subjectDto) {
-        return subjectService.create(subjectDto);
+    ResponseEntity<SubjectDto> create(@RequestBody SubjectDto subjectDto) {
+        return ResponseEntity.ok(subjectService.create(subjectDto));
     }
 
     @PostMapping("/subjects/{subjectId}/ratings")
     @ResponseStatus(code = HttpStatus.CREATED)
-    public RatingDto createRating(@PathVariable Long subjectId, @RequestBody RatingDto ratingDto) {
-        return subjectService.addRating(subjectId, ratingDto);
+    public ResponseEntity<RatingDto> createRating(@PathVariable Long subjectId, @RequestBody RatingDto ratingDto) {
+        return ResponseEntity.ok(subjectService.addRating(subjectId, ratingDto));
     }
 
     @DeleteMapping("/subjects/{subjectId}")
     @ResponseStatus(code = HttpStatus.OK)
-    void delete(@PathVariable Long subjectId) {
+    public ResponseEntity<?> delete(@PathVariable Long subjectId) {
         subjectService.deleteById(subjectId);
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(String.format("Subject[%d] was deleted", subjectId));
     }
 
     @DeleteMapping("/subjects/{subjectId}/ratings/{ratingId}")
     @ResponseStatus(code = HttpStatus.OK)
-    void deleteRating(@PathVariable Long ratingId) {
+    public ResponseEntity<?> deleteRating(@PathVariable Long ratingId) {
         ratingService.deleteById(ratingId);
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(String.format("Rating[%d] was deleted", ratingId));
     }
 
 }
--- src/main/java/com/itech/learning/controller/UserController.java
+++ src/main/java/com/itech/learning/controller/UserController.java
@@ -6,6 +6,7 @@ import com.itech.learning.service.RatingService;
 import com.itech.learning.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -17,40 +18,43 @@ public class UserController {
     private final RatingService ratingService;
 
     @GetMapping("/users")
-    public List<UserDto> getAll() {
-        return userService.getAll();
+    public ResponseEntity<List<UserDto>> getAll() {
+        return ResponseEntity.ok(userService.getAll());
     }
 
     @GetMapping("/users/{userId}")
-    public UserDto getById(@PathVariable Long userId) {
-        return userService.findDtoById(userId);
+    public ResponseEntity<UserDto> getById(@PathVariable Long userId) {
+        return ResponseEntity.ok(userService.findDtoById(userId));
     }
 
     @GetMapping("/users/{userId}/ratings")
-    public List<RatingDto> getUserRatings(@PathVariable Long userId) {
-        return ratingService.findAllByUserId(userId);
+    public ResponseEntity<List<RatingDto>> getUserRatings(@PathVariable Long userId) {
+        return ResponseEntity.ok(ratingService.findAllByUserId(userId));
     }
 
     @GetMapping("/users/{userId}/ratings/{ratingId}")
-    public RatingDto getUserRatingById(@PathVariable Long userId, @PathVariable Long ratingId) {
-        return userService.getUserRatingById(userId, ratingId);
+    public ResponseEntity<RatingDto> getUserRatingById(@PathVariable Long userId, @PathVariable Long ratingId) {
+        return ResponseEntity.ok(userService.getUserRatingById(userId, ratingId));
     }
 
     @PutMapping("/users/{userId}")
-    public UserDto update(@PathVariable Long userId, @RequestBody UserDto userDto) {
-        return userService.update(userId, userDto);
+    public ResponseEntity<UserDto> update(@PathVariable Long userId, @RequestBody UserDto userDto) {
+        return ResponseEntity.ok(userService.update(userId, userDto));
     }
 
     @PostMapping("/users")
     @ResponseStatus(code = HttpStatus.CREATED)
-    UserDto create(@RequestBody UserDto userDto) {
-        return userService.create(userDto);
+    ResponseEntity<UserDto> create(@RequestBody UserDto userDto) {
+        return ResponseEntity.ok(userService.create(userDto));
     }
 
     @DeleteMapping("/users/{userId}")
     @ResponseStatus(code = HttpStatus.OK)
-    void delete(@PathVariable Long userId) {
+    public ResponseEntity<?> delete(@PathVariable Long userId) {
         userService.deleteById(userId);
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(String.format("User[%d] was deleted", userId));
     }
 
 }
